b5ed25ef016a74789e657035f23c9e2cea23fd68
Abhranil Naha <abhranilnaha@gmail.com>
2015-02-08 02:37:16 -0800

    first commit

---
+++ src/main/scala/hello/HelloController.scala
@@ -0,0 +1,17 @@
+package hello
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
+
+@Controller
+@EnableAutoConfiguration
+class HelloController {
+
+    @RequestMapping(Array("/"))
+    @ResponseBody
+    def home() : String = {
+    	"Hello World!"
+    }    
+}
