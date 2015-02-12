12e0520194848586c14efdd70736410ff033850e
Abhranil Naha <abhranilnaha@gmail.com>
2015-02-11 23:13:14 -0800

    Added GET Request Method parameter for better understanding.

--- src/main/scala/hello/HelloController.scala
+++ src/main/scala/hello/HelloController.scala
@@ -3,13 +3,13 @@ package hello
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
+import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, ResponseBody}
 
 @Controller
 @EnableAutoConfiguration
 class HelloController {
 
-    @RequestMapping(Array("/"))
+    @RequestMapping(value=Array("/"),method=Array(RequestMethod.GET))
     @ResponseBody
     def home() : String = {
     	"Hello World!"
